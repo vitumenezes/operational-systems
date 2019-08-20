45281366cc71d31e65bb797f25201297431a4e9c
Victor Gomes <victormenezez15@gmail.com>
2019-08-20 16:38:07 -0300

    Just starting to list all processes

---
+++ process.py
@@ -0,0 +1,11 @@
+import psutil
+
+# Iterate over all running process
+for proc in psutil.process_iter():
+    try:
+        # Get process name & pid from process object.
+        processName = proc.name()
+        processID = proc.pid
+        print(processName , '\t\t\t\t::: ', processID)
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
