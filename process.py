28040fb16bd9bb0771a5f25544cab5c5871a18b1
Victor Gomes <victormenezez15@gmail.com>
2019-08-21 16:44:13 -0300

    Ok

--- process.py
+++ process.py
@@ -1,11 +1,37 @@
-import psutil
+import psutil, pprint, time, os
 
-# Iterate over all running process
-for proc in psutil.process_iter():
-    try:
-        # Get process name & pid from process object.
-        processName = proc.name()
-        processID = proc.pid
-        print(processName , '\t\t\t\t::: ', processID)
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
+def getListOfProcessSortedByMemory():
+    '''
+    Get list of running process sorted by Memory Usage
+    '''
+    listOfProcObjects = []
+    # Iterate over the list
+    for proc in psutil.process_iter():
+       try:
+           # Fetch process details as dict
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+           # pprint.pprint(pinfo)
+           pinfo['vms'] = proc.memory_info().vms / (1024 * 1024)
+           # Append dict to list
+           listOfProcObjects.append(pinfo);
+       except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+           pass
+
+    # Sort list of dict by key vms i.e. memory usage
+    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
+
+    return listOfProcObjects
+
+
+def main():
+    while True:
+        listOfRunningProcess = getListOfProcessSortedByMemory()
+        print('MEMORYUSAGE\tPID\tUSERNAME\tNAME')
+        for elem in listOfRunningProcess[:15] :
+            print(elem['vms'], '\t', elem['pid'], '\t', elem['username'], '\t', elem['name'])
+        time.sleep(1)
+        os.system('clear')
+
+
+if __name__ == '__main__':
+    main()
