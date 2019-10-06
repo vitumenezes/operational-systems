cb35f5fdb6d6e0f498948a8f434e12d5c58645b7
Victor Gomes <victormenezez15@gmail.com>
2019-10-06 17:33:44 -0300

    Added some comments

--- process.py
+++ process.py
@@ -4,26 +4,23 @@ import time
 import os
 import signal
 
+
 def getListOfProcessSortedByMemory():
     '''
-    Get list of running process sorted by Memory Usage
+    Recupera a lista de processos do sistema
     '''
-    listOfProcObjects = []
-    # Iterate over the list
+    process_list = []
+    # iteracao sobre todos os processos
     for proc in psutil.process_iter():
        try:
-           # Fetch process details as dict
            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-           # pprint.pprint(pinfo)
            pinfo['vms'] = proc.memory_info().vms / (1024 * 1024)
-           # Append dict to list
-           listOfProcObjects.append(pinfo);
+           process_list.append(pinfo);
        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
            pass
 
-    # Sort list of dict by key vms i.e. memory usage
-    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
-    return listOfProcObjects
+    process_list = sorted(process_list, key=lambda procObj: procObj['vms'], reverse=True)
+    return process_list
 
 
 def getProcess():
@@ -32,7 +29,7 @@ def getProcess():
     '''
     # lista de processos
     listOfRunningProcess = getListOfProcessSortedByMemory()
-
+    # filtrei apenas pelos com python3, assim ficaria mais visivel a duplicacao
     listProc = (elem for elem in listOfRunningProcess if elem['name'] == 'python3')
 
     os.system('clear')
@@ -40,26 +37,36 @@ def getProcess():
     print('MEMORYUSAGE\tPID\tUSERNAME\tNAME')
     for elem in listProc:
         print(elem['vms'], '\t', elem['pid'], '\t', elem['username'], '\t', elem['name'])
+
     # killProcess()
 
 
 def killProcess():
+    '''
+    Mata um processo
+    '''
     process = input('\nDigite o PID do processo:')
     os.kill(int(process), signal.SIGKILL)
     getProcess()
 
 
 def processFunc():
-    for i in range(2):
-        time.sleep(2)
+    '''
+    Fork a cada 10 segundos. O filho chama a funcao novamente
+    '''
+    for i in range(3):
+        time.sleep(10)
         pid = os.fork()
         if pid > 0:
             continue
         else:
             processFunc()
+    # Ao final, morre
+    exit()
 
 
 def main():
+    getProcess()
     pid = os.fork()
     if pid < 0:
         print("Erro")
@@ -70,8 +77,15 @@ def main():
 
 
 if __name__ == '__main__':
-    getProcess()
     main()
+    # Ainda faltam alguns tratamentos de excecoes
     while True:
-        time.sleep(1)
-        getProcess()
+        print('\n1 - Atualizar\n2 - Matar processo')
+        opc = input('\nDigite a opção: ')
+        print(opc)
+        if opc == '1':
+            getProcess()
+        elif opc == '2':
+            killProcess()
+        else:
+            continue
