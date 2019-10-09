e4014af76c34b1a48ae053b511d89c90fede1cef
Antonio Marcos <anthoin@ufrn.edu.br>
2019-10-09 20:16:22 -0300

    Some changes

--- memory-manager/memory-manager.py
+++ memory-manager/memory-manager.py
@@ -12,6 +12,13 @@ def get_safe_list(list, index):
         return None
 
 
+def swap_process(new_process):
+    global memory_size
+    old_processs = memory.pop(0)
+    memory.append(new_process)
+    memory_size -= old_processs[1]
+
+
 def memory_time(lock):
     while len(memory) > 0:
         lock.acquire()
@@ -27,6 +34,7 @@ def memory_time(lock):
 
 
 def rewrite_hd(processes):
+    print('reescrito')
     with open('hdd.txt', 'w') as hd:
         hd.writelines(processes)
 
@@ -52,19 +60,38 @@ def fill_memory():
         memory_size += int(get_safe_list(process, 1))
 
 
-def memory_checker():
+def memory_checker(lock):
+    print("PASSAGEM")
+    lock.acquire()
+    time.sleep(1)
+    aux_list = []
     with open('hdd.txt', 'r') as hd:
-        process = hd.readline()
-        rewrite_hd(list(hd))
-        return process
+        for line in hd:
+            if line == '':
+                print("Arquivo encerrado")
+                break;
+            
+            process = list(line)
+
+            if int(process[6]) == 0:
+                swap_process(list(map(int, process)))
+            else:
+                result = str(int(line[6]) - 1)
+                process[6] = result
+                aux_list.append(''.join(process))
+            
+    rewrite_hd(aux_list)
+    lock.release()
+    
+    memory_checker(lock)
 
 
-def memory_manager():
+def memory_manager(lock):
     fill_memory()
-
+    memory_checker(lock)
 
 if __name__ == "__main__":
-    memory_manager()
     lock = Lock()
+    memory_manager(lock)
     memory_time_checker = Thread(target=memory_time, args=(lock,))
     memory_time_checker.start()
